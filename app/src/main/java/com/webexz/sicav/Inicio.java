9600459e09ad323f8738b8a3e36d1ce0168ea605
WEBEXZ <audi.icon@gmail.com>
2016-11-17 21:57:38 -0600

    Version 2.0

--- app/src/main/java/com/webexz/sicav/Inicio.java
+++ app/src/main/java/com/webexz/sicav/Inicio.java
@@ -1,10 +1,25 @@
 package com.webexz.sicav;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Inicio extends AppCompatActivity {
 
@@ -12,17 +27,27 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
         Button comenzar = (Button) findViewById(R.id.menu);
+        if(!redActiva()) {
+            Toast.makeText(getApplicationContext(), "SIN CONEXIÓN AL SERVIDOR", Toast.LENGTH_SHORT).show();
+        }
         comenzar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lanzarMenu();
+                lanzarLogin();
             }
         });
     }
 
-    public void lanzarMenu() {
-        Intent menu = new Intent(getApplicationContext(), Menu.class);
-        startActivity(menu);
+    public void lanzarLogin() {
+        Intent login = new Intent(getApplicationContext(), Login.class);
+        startActivity(login);
+    }
+
+    public boolean redActiva(){
+        ConnectivityManager conexion = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo informacion = conexion.getActiveNetworkInfo();
+        return (informacion.isConnected() && informacion != null);
     }
 }
